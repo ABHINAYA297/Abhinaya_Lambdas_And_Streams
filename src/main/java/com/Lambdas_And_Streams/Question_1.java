dca7c85452328b69df84d7ad3c8259e3686e197b
ABHINAYA297 <abhinayarani225@gmail.com>
2020-07-26 10:30:03 +0530

    Lambdas and Streams

---
+++ src/main/java/com/Lambdas_And_Streams/Question_1.java
@@ -0,0 +1,18 @@
+package com.Lambdas_And_Streams;
+
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+
+public class Question_1{
+	public static void main(String args[]) {
+		IntStream stream=IntStream.of(10,15,20,30,45,50,70,80,90);
+		OptionalDouble obj=stream.average();
+		if(obj.isPresent()) {
+			System.out.println("Stream Average: "+obj.getAsDouble());
+		}else {
+			System.out.println(-1);
+		}
+		
+	}
+	
+}
---
+++ src/test/java/com/Lambdas_And_Streams/AppTest.java
@@ -0,0 +1,19 @@
+package com.Lambdas_And_Streams;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    
+    /**
+     * Rigorous Test :-)
+     */
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
